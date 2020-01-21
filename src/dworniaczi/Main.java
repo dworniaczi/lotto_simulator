df754994d114f3b6087a6347c4456250b59f20bd
Tomek <tomasz.dworniczak@hotmail.com>
2020-01-22 00:16:20 +0100

    Created filters: input type and input range

--- src/dworniaczi/Main.java
+++ src/dworniaczi/Main.java
@@ -12,11 +12,21 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Witaj! Zagrajmy w lotka. Podaj 6 liczb w zakresie 1 - 49.");
-        int[] playerArray = new int[6];
+        String[] playerArray = new String[6];
 
             for (int i = 0; i < playerArray.length; i++) {
                 System.out.println("Podaj liczbę");
-                playerArray[i] = scanner.nextInt();
+                String input = scanner.next();
+                try {
+                    int num = Integer.parseInt(input);
+                    if (num >= 1 && num <= 49) {
+                        playerArray[i] = input;
+                    } else {
+                        System.out.println("Liczba spoza zakresu!");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("To nie liczba!");
+                }
             }
         System.out.println(Arrays.toString(playerArray));
         }
