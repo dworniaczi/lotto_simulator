58d62bb0a37c9d9447befe7f4b14b1ba4d843cdc
Tomek <tomasz.dworniczak@hotmail.com>
2020-01-22 15:50:27 +0100

    Added random numbers generator, with filter that doesn't allow duplicates

--- src/dworniaczi/Main.java
+++ src/dworniaczi/Main.java
@@ -1,16 +1,14 @@
 package dworniaczi;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Witaj! Zagrajmy w lotka. Podaj 6 liczb w zakresie 1 - 49.");
-        List<String> playerArray = new ArrayList<String>(6);
+        List<String> playerList = new ArrayList<String>(6);
 
         for (int i = 0; i < 6;) {
             System.out.println("Podaj liczbę");
@@ -18,8 +16,8 @@ public class Main {
             try {
                 int num = Integer.parseInt(input);
                 if (num >= 1 && num <= 49) {
-                    if (!playerArray.contains(input)) {
-                        playerArray.add(input);
+                    if (!playerList.contains(input)) {
+                        playerList.add(input);
                         i++;
                     } else {
                         System.out.println("Liczby nie mogą się powtarzać!");
@@ -30,8 +28,19 @@ public class Main {
             } catch (NumberFormatException e) {
                 System.out.println("To nie liczba!");
             }
-            System.out.println(playerArray);
+            System.out.println(playerList);
         }
+
+        Random random = new Random();
+        List<Integer> computerList = new ArrayList<Integer>(6);
+        for (int i = 0; i < 6;) {
+            int randomNum = random.nextInt(50) + 1;
+            if (!computerList.contains(randomNum)) {
+                computerList.add(randomNum);
+                i++;
+            }
+        }
+        System.out.println(computerList);
     }
 }
 
